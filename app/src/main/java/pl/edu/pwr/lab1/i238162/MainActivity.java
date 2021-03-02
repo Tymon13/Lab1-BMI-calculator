fbd8bd574158145b32895eea324fc67072984fa1
Kamil Tymiński <kamil_tym@wp.pl>
2021-03-02 23:19:21 +0100

    Create empty project

---
+++ app/src/main/java/pl/edu/pwr/lab1/i238162/MainActivity.java
@@ -0,0 +1,14 @@
+package pl.edu.pwr.lab1.i238162;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
